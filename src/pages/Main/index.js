b22bf466120a83d5f6c2a544fb2cd275198e8bdd
Alex Aguiar <alex_aguiar13@live.com>
2019-04-25 02:15:53 -0300

    Aplicação frontend

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
-import api from '../../services/api';
+import React, { Component } from "react";
+import api from "../../services/api";
 
 import logo from "../../assets/logo.jpg";
 import "./styles.css";
 
 export default class Main extends Component {
-   state ={
-      newBox: ""
-   };
+    state = {
+        newBox: ""
+    };
 
-   handleSubmit = async e => {
-      e.preventDefault();
-      const response = await api.post("boxes", {
-         title: this.state.newBox,
-      });
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await api.post("boxes", {
+            title: this.state.newBox
+        });
 
-      this.props.history.push(`/box/${response.data_id}`);
-   };
+        this.props.history.push(`/box/${response.data._id}`);
+    };
 
-   handleInputChange = e => {
-      this.setState({ newBox: e.target.value });
-   };
+    handleInputChange = e => {
+        this.setState({ newBox: e.target.value });
+    };
 
-   render() {
-      return (
-         <div id="main-container">
-            <img src={logo} alt="" />
-            <form onSubmit={this.handleSubmit}>              
-               <input 
-                  placeholder="Criar uma box"
-                  value={this.state.newBox}
-                  onChange={this.handleInputChange}
-                />
-               <button type="submit">Criar</button>
-            </form>
-         </div>
-      );
-   }
+    render() {
+        return (
+            <div id="main-container">
+                <form onSubmit={this.handleSubmit}>
+                    <img src={logo} alt="" />
+                    <input
+                        placeholder="Criar uma box"
+                        value={this.state.newBox}
+                        onChange={this.handleInputChange}
+                    />
+                    <button type="submit">Criar</button>
+                </form>
+            </div>
+        );
+    }
 }
