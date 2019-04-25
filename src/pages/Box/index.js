efb8a6558f9d67bd1e790f6388a1ae304929238c
Alex Aguiar <alex_aguiar13@live.com>
2019-04-25 18:12:14 -0300

    Aplicação front-end

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -11,87 +11,76 @@ import logo from "../../assets/logo.jpg";
 import "./styles.css";
 
 export default class Box extends Component {
-    state = { box: {} };
+  state = { box: {} };
 
-    async componentDidMount() {
-        this.subscribeToNewFiles();
-        const box = this.props.match.params.id;
-        const response = await api.get(`boxes/${box}`);
+  async componentDidMount() {
+    this.subscribeToNewFiles();
+    const box = this.props.match.params.id;
+    const response = await api.get(`boxes/${box}`);
 
-        this.setState({ box: response.data });
-    }
+    this.setState({ box: response.data });
+  }
 
-    subscribeToNewFiles = () => {
-        const box = this.props.match.params.id;
-        const io = socket("https://gaveta-backend.herokuapp.com");
+  subscribeToNewFiles = () => {
+    const box = this.props.match.params.id;
+    const io = socket("https://gaveta-backend.herokuapp.com");
 
-        io.emit("connectRoom", box);
-        io.on("file", data => {
-            this.setState({
-                box: {
-                    ...this.state.box,
-                    files: [data, ...this.state.box.files]
-                }
-            });
-        });
-    };
+    io.emit("connectRoom", box);
+    io.on("file", data => {
+      this.setState({
+        box: {
+          ...this.state.box,
+          files: [data, ...this.state.box.files]
+        }
+      });
+    });
+  };
 
-    handleUpload = files => {
-        files.forEach(file => {
-            const data = new FormData();
-            const box = this.props.match.params.id;
+  handleUpload = files => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
 
-            data.append("file", file);
-            api.post(`boxes/${box}/files`, data);
-        });
-    };
+      data.append("file", file);
+      api.post(`boxes/${box}/files`, data);
+    });
+  };
 
-    render() {
-        return (
-            <div id="box-container">
-                <header>
-                    <img src={logo} alt="" />
-                    <h1>{this.state.box.title}</h1>
-                </header>
+  render() {
+    return (
+      <div id="box-container">
+        <header>
+          <img src={logo} alt="" />
+          <h1>{this.state.box.title}</h1>
+        </header>
 
-                <DropZone onDropAccepted={this.handleUpload}>
-                    {({ getRootProps, getInputProps }) => (
-                        <div className="upload" {...getRootProps()}>
-                            <input {...getInputProps()} />
-                            <p>Arraste arquivos ou clique aqui</p>
-                        </div>
-                    )}
-                </DropZone>
-
-                <ul>
-                    {this.state.box.files &&
-                        this.state.box.files.map(file => (
-                            <li key={file._id}>
-                                <a
-                                    className="fileInfo"
-                                    href={file.url}
-                                    target="_blank"
-                                >
-                                    <MdInsertDriveFile
-                                        size={24}
-                                        color="#1e5c57"
-                                    />
-                                    <strong>{file.title}</strong>
-                                </a>
-                                <span>
-                                    há{" "}
-                                    {distanceInWords(
-                                        file.createdAt,
-                                        new Date(),
-                                        {
-                                            locale: pt
-                                        }
-                                    )}
-                                </span>
-                            </li>
-                        ))}
-                </ul>
+        <DropZone onDropAccepted={this.handleUpload}>
+          {({ getRootProps, getInputProps }) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Arraste arquivos ou clique aqui</p>
             </div>
-        );
-    }
+          )}
+        </DropZone>
+
+        <ul>
+          {this.state.box.files &&
+            this.state.box.files.map(file => (
+              <li key={file._id}>
+                <a className="fileInfo" href={file.url} target="_blank">
+                  <MdInsertDriveFile size={24} color="#1e5c57" />
+                  <strong>{file.title}</strong>
+                </a>
+                <span>
+                  há{" "}
+                  {distanceInWords(file.createdAt, new Date(), {
+                    locale: pt
+                  })}
+                </span>
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+  }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios  from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://gaveta-backend.herokuapp.com',
+  baseURL: "https://gaveta-backend.herokuapp.com"
 });
 
 export default api;
